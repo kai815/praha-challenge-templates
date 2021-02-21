cdb37ed2a60ce34c7dee2a178169d32fffc0d906
kai815 <h.kaizuka815@gmail.com>
2021-02-21 13:03:47 +0900

    課題2のfunction.tsのテスト

--- jestSample/__tests__/functions.test.ts
+++ jestSample/__tests__/functions.test.ts
@@ -1,1 +1,159 @@
-// todo: ここに単体テストを書いてみましょう！
+import {
+  sumOfArray,
+  asyncSumOfArray,
+  asyncSumOfArraySometimesZero,
+  getFirstNameThrowIfLong,
+} from "../functions";
+import { DatabaseMock } from "../util";
+import { NameApiService } from "../nameApiService";
+
+describe("sumOfArray", (): void => {
+  // normal number
+  it("input [1,1] output 2", (): void => {
+    expect(sumOfArray([1, 1])).toBe(2);
+  });
+
+  // including -
+  it("input [1,2,3,4,5,-6] output 21", (): void => {
+    expect(sumOfArray([1, 2, 3, 4, 5, -6])).toBe(9);
+  });
+
+  // including decimal
+  it("input [1,2,3,4,5,0.5] output 15.5", (): void => {
+    expect(sumOfArray([1, 2, 3, 4, 5, 0.5])).toBe(15.5);
+  });
+
+  // exception
+  it("input [] throw Exception", (): void => {
+    expect((): void => {
+      sumOfArray([]);
+    }).toThrow();
+  });
+});
+
+describe("asyncSumOfArray", (): void => {
+  // normal number
+  it("input [1,1] output 2", async (): Promise<void> => {
+    const data = await asyncSumOfArray([1, 1]);
+    expect(data).toBe(2);
+  });
+
+  // inucluding -
+  it("input [1,2,3,4,5,-6] output 9", async (): Promise<void> => {
+    const data = await asyncSumOfArray([1, 2, 3, 4, 5, -6]);
+    expect(data).toBe(9);
+  });
+
+  // including decimal
+  it("input [1,2,3,4,5,0.5] output 15.5", async (): Promise<void> => {
+    const data = await asyncSumOfArray([1, 2, 3, 4, 5, 0.5]);
+    expect(data).toBe(15.5);
+  });
+  // exception
+  it("input [] throw Exception", (): void => {
+    expect(asyncSumOfArray([])).rejects.toThrow();
+  });
+  // exception
+});
+
+describe("asyncSumOfArraySometimesZero", (): void => {
+  describe("DatabaseMockSaveMethod Normal", (): void => {
+    const databaseNormal = new DatabaseMock();
+    databaseNormal.save = jest.fn();
+    // normal number
+    it("input [1,1] output 2", async (): Promise<void> => {
+      const data = await asyncSumOfArraySometimesZero(databaseNormal, [1, 1]);
+      expect(data).toBe(2);
+    });
+    // inucluding -
+    it("input [1,2,3,4,5,-6] output 9", async (): Promise<void> => {
+      const data = await asyncSumOfArraySometimesZero(databaseNormal, [
+        1,
+        2,
+        3,
+        4,
+        5,
+        -6,
+      ]);
+      expect(data).toBe(9);
+    });
+
+    // including decimal
+    it("input [1,2,3,4,5,0.5] output 15.5", async (): Promise<void> => {
+      const data = await asyncSumOfArraySometimesZero(databaseNormal, [
+        1,
+        2,
+        3,
+        4,
+        5,
+        0.5,
+      ]);
+      expect(data).toBe(15.5);
+    });
+  });
+
+  describe("DatabaseMockSaveMethod throw Error", (): void => {
+    const databaseNormal = new DatabaseMock();
+    databaseNormal.save = jest.fn((): void => {
+      throw new Error("fail!");
+    });
+    // normal number
+    it("input [1,1] output 0", async (): Promise<void> => {
+      const data = await asyncSumOfArraySometimesZero(databaseNormal, [1, 1]);
+      expect(data).toBe(0);
+    });
+    // inucluding -
+    it("input [1,2,3,4,5,-6] output 0", async (): Promise<void> => {
+      const data = await asyncSumOfArraySometimesZero(databaseNormal, [
+        1,
+        2,
+        3,
+        4,
+        5,
+        -6,
+      ]);
+      expect(data).toBe(0);
+    });
+
+    // including decimal
+    it("input [1,2,3,4,5,0.5] output 0", async (): Promise<void> => {
+      const data = await asyncSumOfArraySometimesZero(databaseNormal, [
+        1,
+        2,
+        3,
+        4,
+        5,
+        0.5,
+      ]);
+      expect(data).toBe(0);
+    });
+  });
+});
+
+describe("getFirstNameThrowIfLong", (): void => {
+  const nameApiService = new NameApiService();
+  nameApiService.getFirstName = jest.fn(
+    (): Promise<string> => {
+      return new Promise((resolve): void => {
+        resolve("HIDE");
+      });
+    }
+  );
+
+  it("if maxNameLength more than firstNameLength return 'HIDE'", async (): Promise<
+    void
+  > => {
+    const data = await getFirstNameThrowIfLong(nameApiService, 5);
+    expect(data).toBe("HIDE");
+  });
+
+  it("if maxNameLength less than firstNameLength throw error", async (): Promise<
+    void
+  > => {
+    expect(
+      async (): Promise<void> => {
+        await getFirstNameThrowIfLong(nameApiService, 2);
+      }
+    ).rejects.toThrow("first_name too long");
+  });
+});
--- jestSample/functions.ts
+++ jestSample/functions.ts
@@ -12,11 +12,12 @@ export const asyncSumOfArray = (numbers: number[]): Promise<number> => {
 };
 
 export const asyncSumOfArraySometimesZero = (
+  database: DatabaseMock,
   numbers: number[]
 ): Promise<number> => {
   return new Promise((resolve): void => {
     try {
-      const database = new DatabaseMock(); // fixme: この関数をテストするには、DatabaseMockの使い方を変える必要がありそう！ヒント：依存性の注入
+      // fixme: この関数をテストするには、DatabaseMockの使い方を変える必要がありそう！ヒント：依存性の注入
       database.save(numbers);
       resolve(sumOfArray(numbers));
     } catch (error) {
@@ -26,10 +27,11 @@ export const asyncSumOfArraySometimesZero = (
 };
 
 export const getFirstNameThrowIfLong = async (
+  nameApiService: NameApiService,
   maxNameLength: number
 ): Promise<string> => {
-  const nameApiSerivce = new NameApiService(); // fixme: この関数をテストするには、NameApiServiceの使い方を変える必要がありそう！ヒント：依存性の注入
-  const firstName = await nameApiSerivce.getFirstName();
+  // fixme: この関数をテストするには、NameApiServiceの使い方を変える必要がありそう！ヒント：依存性の注入
+  const firstName = await nameApiService.getFirstName();
 
   if (firstName.length > maxNameLength) {
     throw new Error("first_name too long");
