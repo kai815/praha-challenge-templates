4d80c826872dcb85abb70480590c0619dc10ede1
kai815 <h.kaizuka815@gmail.com>
2021-05-24 07:02:01 +0900

    multiplyの2のテストの通過

--- jestSample/__tests__/tdd/multiply.test.ts
+++ jestSample/__tests__/tdd/multiply.test.ts
@@ -1,7 +1,12 @@
 import { multiply } from "../../tdd/multiply";
+import { thirtyLength } from "./common";
 
 describe("add関数のテスト", (): void => {
   it("1,10,2,を引数に渡すと 20 が返ってくる", (): void => {
     expect(multiply(1, 10, 2)).toBe(20);
+  });
+
+  it("1 を 30 個、引数を渡すと 1 が返ってくる", (): void => {
+    expect(multiply(...thirtyLength)).toBe(1);
   });
 });
