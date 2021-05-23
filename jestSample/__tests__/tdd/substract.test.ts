b247f17f487e55e8627935a9729c88d8be65a22d
kai815 <h.kaizuka815@gmail.com>
2021-05-23 15:52:13 +0900

    substractの2つめのテストの追加

--- jestSample/__tests__/tdd/substract.test.ts
+++ jestSample/__tests__/tdd/substract.test.ts
@@ -1,7 +1,12 @@
 import { substract } from "../../tdd/substract";
+import { twentyNineLength } from "./common";
 
 describe("substract関数のテスト", (): void => {
   it("10,3,2 を渡すと 5 が返ってくる", (): void => {
     expect(substract(10, 3, 2)).toBe(5);
+  });
+
+  it("100 と 1 を 29 個、引数を渡すと 71 が返ってくる", (): void => {
+    expect(substract(100, ...twentyNineLength)).toBe(71);
   });
 });
