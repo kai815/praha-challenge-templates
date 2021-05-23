f5e44fb71f2c6a107acfd0e66704f5d9aa9208f5
kai815 <h.kaizuka815@gmail.com>
2021-05-23 15:09:19 +0900

    add関数の2つめのテストの実装

--- jestSample/__tests__/tdd/add.test.ts
+++ jestSample/__tests__/tdd/add.test.ts
@@ -1,7 +1,12 @@
 import { add } from "../../tdd/add";
+import { thirtyLength } from "./common";
 
 describe("add関数のテスト", (): void => {
   it("3と10と3を引数に渡すと16が返ってくる", (): void => {
     expect(add(3, 10, 3)).toBe(16);
+  });
+
+  it("1を30個渡す30が返ってくる", (): void => {
+    expect(add(...thirtyLength)).toBe(30);
   });
 });
--- jestSample/__tests__/tdd/common.ts
+++ jestSample/__tests__/tdd/common.ts
@@ -1,1 +1,3 @@
 export const thirtyOneLength: number[] = [...Array(31)].map((): number => 1);
+
+export const thirtyLength: number[] = [...Array(30)].map((): number => 1);
