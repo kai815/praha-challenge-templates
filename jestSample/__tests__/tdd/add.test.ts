cfd3384068772bc6367f356444b501b542b33fa7
kai815 <h.kaizuka815@gmail.com>
2021-05-23 15:15:04 +0900

    add関数の3のテストコード

--- jestSample/__tests__/tdd/add.test.ts
+++ jestSample/__tests__/tdd/add.test.ts
@@ -9,4 +9,8 @@ describe("add関数のテスト", (): void => {
   it("1を30個渡す30が返ってくる", (): void => {
     expect(add(...thirtyLength)).toBe(30);
   });
+
+  it("1000,1,2 を引数に渡すと「too big」が返ってくる", (): void => {
+    expect(add(1000, 1, 2)).toBe("too big");
+  });
 });
