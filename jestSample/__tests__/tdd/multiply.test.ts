e98b2c6953d1e71627e092f96aaa7ceb08487560
kai815 <h.kaizuka815@gmail.com>
2021-05-24 07:04:55 +0900

    multiplyの3つめのテスト

--- jestSample/__tests__/tdd/multiply.test.ts
+++ jestSample/__tests__/tdd/multiply.test.ts
@@ -9,4 +9,8 @@ describe("add関数のテスト", (): void => {
   it("1 を 30 個、引数を渡すと 1 が返ってくる", (): void => {
     expect(multiply(...thirtyLength)).toBe(1);
   });
+
+  it("1001,1 を引数に渡すと「big big number」", (): void => {
+    expect(multiply(1001, 1)).toBe("big big number");
+  });
 });
