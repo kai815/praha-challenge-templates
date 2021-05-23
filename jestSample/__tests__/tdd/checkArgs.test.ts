655a241f056725f9bed3bfad2a98696a2d14660d
kai815 <h.kaizuka815@gmail.com>
2021-05-23 14:32:43 +0900

    31個の引数を渡すときにエラーになるテストの追加

--- jestSample/__tests__/tdd/checkArgs.test.ts
+++ jestSample/__tests__/tdd/checkArgs.test.ts
@@ -1,7 +1,14 @@
 import { checkArgs } from "../../tdd/checkArgs";
+import { thirtyOneLength } from "./common";
 
 describe("checkArgs関数のテスト", (): void => {
   it("1と2を引数に渡すとtrueが返ってくる", (): void => {
     expect(checkArgs(1, 2)).toBe(true);
+  });
+
+  it("1と31個を引数に渡すとエラーになる", (): void => {
+    expect(checkArgs(...thirtyOneLength)).toThrow(
+      "引数をしているできるのは30個までです。"
+    );
   });
 });
