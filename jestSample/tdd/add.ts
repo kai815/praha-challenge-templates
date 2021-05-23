1a230bce618dfd8c4f3b5db29bc0d838819cb1af
kai815 <h.kaizuka815@gmail.com>
2021-05-23 15:18:56 +0900

    add関数の3のテストを通過

--- jestSample/tdd/add.ts
+++ jestSample/tdd/add.ts
@@ -1,10 +1,10 @@
 import { checkArgs } from "./checkArgs";
 
-export const add = (...args: number[]): number | void => {
-  // 空の関数
+export const add = (...args: number[]): number | void | string => {
   if (checkArgs(...args)) {
-    return args.reduce(
+    const result = args.reduce(
       (accumulator, currentValue): number => accumulator + currentValue
     );
+    return result > 1000 ? "too big" : result;
   }
 };
