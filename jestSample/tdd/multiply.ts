805ccffbdc660dcdf008bdf818e626a2afe0dd86
kai815 <h.kaizuka815@gmail.com>
2021-05-23 17:27:03 +0900

    multiplyのテストを通す

--- jestSample/tdd/multiply.ts
+++ jestSample/tdd/multiply.ts
@@ -1,5 +1,9 @@
 import { checkArgs } from "./checkArgs";
 
-export const multiply = (...args: number[]): void => {
-  //空の関数
+export const multiply = (...args: number[]): void | number => {
+  if (checkArgs(...args)) {
+    return args.reduce(
+      (accumulator, currentValue): number => accumulator * currentValue
+    );
+  }
 };
