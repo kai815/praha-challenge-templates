5fed9efce77bc02dafa7fcb4e7db393985eb02f2
kai815 <h.kaizuka815@gmail.com>
2021-05-24 11:43:27 +0900

    divideの3つめのテストの通過

--- jestSample/__tests__/tdd/dvide.test.ts
+++ jestSample/__tests__/tdd/dvide.test.ts
@@ -4,7 +4,12 @@ describe("divide関数のテスト", (): void => {
   it("10,5 を引数に渡すと 2 が返ってくる", (): void => {
     expect(divide(10, 5)).toBe(2);
   });
+
   it("10,3,3 を引数に渡すと 1.1 が返ってくる", (): void => {
     expect(divide(10, 3, 3)).toBe(1.1);
+  });
+
+  it("1,4 を引数に渡すと、0.3 が返ってくる", (): void => {
+    expect(divide(1, 4)).toBe(0.3);
   });
 });
